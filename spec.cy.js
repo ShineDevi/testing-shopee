afcae224cf42edc07d13665a22cee4a33be0b71a
ShineDevi <shinedevi02@gmail.com>
2022-11-15 23:00:13 +0700

    login

--- spec.cy.js
+++ spec.cy.js
@@ -1,9 +1,9 @@
 describe('empty spec', () => {
   it('passes', () => {
     cy.visit('https://shopee.co.id')
-    cy.contains('Log In').click()
-    cy.contains('Google').click()
     cy.get('input[class="shopee-searchbar-input__input"]').type('sepatu')
     cy.get('button[class="btn btn-solid-primary btn--s btn--inline shopee-searchbar__search-button"]').click()
+    cy.contains('Log In').click()
+    cy.contains('Google').click()
   })
 })
